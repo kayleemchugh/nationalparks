574ada038d06c2c9cd67a1cac868cf131965f2a9
Kaylee <kaylee.mchugh@credera.com>
2019-04-10 09:32:03 -0600

    files from spring initializer

---
+++ src/main/java/bootcamp/nationalparks/nationalparks/NationalparksApplication.java
@@ -0,0 +1,13 @@
+package bootcamp.nationalparks.nationalparks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationalparksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NationalparksApplication.class, args);
+	}
+
+}
