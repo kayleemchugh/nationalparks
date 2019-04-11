7d9ded2891e1fd49f7aea36e461f7319e2ae0419
Kaylee <kaylee.mchugh@credera.com>
2019-04-11 15:58:14 -0600

    basic service request endpoints have been written

--- src/main/java/sample/projects/nationalparks/controller/ServiceRequestController.java
+++ src/main/java/sample/projects/nationalparks/controller/ServiceRequestController.java
@@ -1,5 +1,54 @@
 package sample.projects.nationalparks.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import sample.projects.nationalparks.model.ServiceRequest;
+import sample.projects.nationalparks.service.ServiceRequestService;
+
+@RestController
+@RequestMapping("/serviceRequest")
 public class ServiceRequestController
 {
+
+   private final ServiceRequestService serviceRequestService;
+
+   @Autowired
+   public ServiceRequestController(ServiceRequestService serviceRequestService) {
+      this.serviceRequestService = serviceRequestService;
+   }
+
+   @GetMapping("/{id}")
+   public ServiceRequest getServiceRequest(@PathVariable(value="id") Integer id) {
+      ServiceRequest serviceRequest = serviceRequestService.getServiceRequestById(id);
+      if(serviceRequest == null) {
+         throw new Error("could not find service request with this id");
+      }
+      return serviceRequest;
+   }
+
+   @PostMapping
+   public ServiceRequest saveServiceRequest(@RequestBody ServiceRequest serviceRequest) {
+      return serviceRequestService.saveServiceRequest(serviceRequest);
+   }
+
+   @PutMapping("/{id}")
+   public ServiceRequest updateServiceRequest(@PathVariable(value="id") Integer id,
+                                @RequestBody ServiceRequest serviceRequest) {
+      return serviceRequestService.updateServiceRequest(id, serviceRequest);
+   }
+
+   // endpoints needed
+   // get service request by service request id
+   // get service request by park
+   // get open service requests by park
+   // get closed service requests by park
+   // get service request by visitor id
+   // get service request by employee id
+   // update service request
+   // get unanswered service requests
+   // get closed service requests
+   // close service request
+
+
+
 }
--- src/main/java/sample/projects/nationalparks/service/ServiceRequestService.java
+++ src/main/java/sample/projects/nationalparks/service/ServiceRequestService.java
@@ -1,8 +1,38 @@
 package sample.projects.nationalparks.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sample.projects.nationalparks.model.ServiceRequest;
+import sample.projects.nationalparks.repository.ServiceRequestRepository;
 
 @Service
 public class ServiceRequestService
 {
+
+   private final ServiceRequestRepository serviceRequestRepository;
+
+   @Autowired
+   public ServiceRequestService(ServiceRequestRepository serviceRequestRepository) {
+      this.serviceRequestRepository = serviceRequestRepository;
+   }
+
+   public ServiceRequest getServiceRequestById(Integer id) {
+      return  serviceRequestRepository.getOne(id);
+   }
+
+   public ServiceRequest saveServiceRequest(ServiceRequest serviceRequest)
+   {
+      return serviceRequestRepository.save(serviceRequest);
+   }
+
+   public ServiceRequest updateServiceRequest(Integer serviceRequestId, ServiceRequest serviceRequest) {
+      if(!serviceRequestId.equals(serviceRequest.getId())) {
+         throw new Error("Please pass in the same park object as the id in your request");
+      }
+
+      return serviceRequestRepository.save(serviceRequest);
+   }
+
+
+
 }
